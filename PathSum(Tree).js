895ec66069c8574876a6856f0122a76fe309e986
Finley Matthews <79953082+finleymatthews96@users.noreply.github.com>
2021-08-02 11:53:33 -0400

    Update PathSum(Tree).js

--- PathSum(Tree).js
+++ PathSum(Tree).js
@@ -1,6 +1,8 @@
 /**
 112. Path Sum
 
+LeetCode Easy
+
 Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
 A leaf is a node with no children.
 
