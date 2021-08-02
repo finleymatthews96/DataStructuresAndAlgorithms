bad59c51b55886426ff0370122e56506160c9721
Finley Matthews <79953082+finleymatthews96@users.noreply.github.com>
2021-08-02 11:51:55 -0400

    Update BestTimetoBuyandSellStockII(Array).js

--- BestTimetoBuyandSellStockII(Array).js
+++ BestTimetoBuyandSellStockII(Array).js
@@ -1,6 +1,8 @@
 /*
 Best Time to Buy and Sell Stock II
 
+LeetCode Easy
+
 You are given an array prices where prices[i] is the price of a given stock on the i-th day.
 Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
 Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
