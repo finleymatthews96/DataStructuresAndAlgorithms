caf5e3f9add7f5bf5a0f3d5e2e0a2d2691aa8f9d
Finley Matthews <79953082+finleymatthews96@users.noreply.github.com>
2021-08-26 15:02:22 -0400

    Create 3Sum(Array).js

---
+++ 3Sum(Array).js
@@ -0,0 +1,52 @@
+/* 3Sum
+LeetCode Medium
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Example 2:
+
+Input: nums = []
+Output: []
+Example 3:
+
+Input: nums = [0]
+Output: []
+
+*/
+
+// sorting solution
+// O(n^2) T | O(1) S (besides answer)
+var threeSum = function(nums) {
+  let result = [];
+  nums.sort((a, b) => a - b)
+  
+  for (let i=0; i<nums.length - 2; i++){
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right){
+      if (nums[left] + nums[right] + nums[i] === 0){
+        result.push([nums[i], nums[left], nums[right]])
+        while (nums[left+1] === nums[left]) left++;
+        while (nums[right-1] === nums[right]) right--;
+        left++;
+        right--;
+      } else if (nums[left] + nums[right] + nums[i] > 0){
+        right--;
+      } else {
+        left++;
+      }
+      while (nums[i+1] === nums[i]) i++;
+    }
+    
+  }
+  
+  return result;
+};
