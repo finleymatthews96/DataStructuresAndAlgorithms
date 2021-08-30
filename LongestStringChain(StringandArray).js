dff5aa0a7e8cb99099d65647bb65848f24b3f3b3
Finley Matthews <79953082+finleymatthews96@users.noreply.github.com>
2021-08-29 21:15:15 -0400

    Update LongestStringChain(StringandArray).js

--- LongestStringChain(StringandArray).js
+++ LongestStringChain(StringandArray).js
@@ -31,3 +31,46 @@ Explanation: The trivial word chain ["abcd"] is one of the longest word chains.
 ["abcd","dbqca"] is not a valid word chain because the ordering of the letters is changed.
 
 */
+
+// Recursion with memoization
+// O(n^2 * m) T || O(n) S where n = length of array, m = longest word in array
+var longestStrChain = function(words) {
+  let memo = {};
+  let longest = 0;
+  words.sort((a,b) => b.length - a.length);
+  
+  for (let i=0; i<words.length; i++){
+    if (!memo[i]) memo[i] = 1 + longestPossible(words, i, memo);
+    if (memo[i] > longest) longest = memo[i];
+  }
+  
+  return longest;
+};
+
+var longestPossible = function(words, i, memo){
+  let current = 0;
+  
+  for (let j=i+1; j<words.length; j++){
+    if (isPredecessor(words[j], words[i])){
+      if (!memo[j]) memo[j] = 1 + longestPossible(words, j, memo)
+      if (memo[j] > current) current = memo[j];
+    }
+  }
+  
+  return current;
+}
+
+var isPredecessor = function (predecessor, successor){
+  if (successor.length - 1 !== predecessor.length) return false;
+  
+  let successorIdx = 0;
+  for (let i=0; i < predecessor.length; i++){
+    while (predecessor[i] !== successor[successorIdx] && successorIdx < successor.length){
+      successorIdx++;
+    }
+    if (predecessor[i] !== successor[successorIdx]) return false;
+    successorIdx++;
+  }
+  
+  return true;
+}
