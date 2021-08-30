c4547cc7a418d3722122918f47dd0de7d589a5b2
Finley Matthews <79953082+finleymatthews96@users.noreply.github.com>
2021-08-29 21:31:21 -0400

    Update LongestStringChain(StringandArray).js

--- LongestStringChain(StringandArray).js
+++ LongestStringChain(StringandArray).js
@@ -74,3 +74,39 @@ var isPredecessor = function (predecessor, successor){
   
   return true;
 }
+
+// dynamic programming with reference array
+// O(n^2 * m) T | O(n) S
+var longestStrChain = function(words) {
+  let stepsToGetHere = Array(words.length).fill(0);
+  let longest = 0;
+  words.sort((a, b) => a.length - b.length)
+  
+  for (let i=0; i<words.length; i++){
+    if (longest === 0) longest = 1;
+    let currentCount = stepsToGetHere[i] || 1;
+    for (let j = i+1; j<words.length; j++){
+      if (isPredecessor(words[i], words[j])){
+        stepsToGetHere[j] = Math.max(stepsToGetHere[j], currentCount + 1)
+        if (stepsToGetHere[j] > longest) longest = stepsToGetHere[j]
+      }
+    }
+  }
+
+  return longest;
+}
+
+var isPredecessor = function (predecessor, successor){
+  if (successor.length - 1 !== predecessor.length) return false;
+  
+  let successorIdx = 0;
+  for (let i=0; i < predecessor.length; i++){
+    while (predecessor[i] !== successor[successorIdx] && successorIdx < successor.length){
+      successorIdx++;
+    }
+    if (predecessor[i] !== successor[successorIdx]) return false;
+    successorIdx++;
+  }
+  
+  return true;
+}
