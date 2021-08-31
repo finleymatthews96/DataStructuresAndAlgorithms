899cb7139b490af13ef10534adfe3a494102a5bb
Finley Matthews <79953082+finleymatthews96@users.noreply.github.com>
2021-08-31 12:58:14 -0400

    Create Merging2Packages(array).js

---
+++ Merging2Packages(array).js
@@ -0,0 +1,28 @@
+/* Merging 2 Packages
+Pramp Easy
+
+Given a package with a weight limit limit and an array arr of item weights, implement a function getIndicesOfItemWeights 
+that finds two items whose sum of weights equals the weight limit limit. Your function should return a pair [i, j] of the 
+indices of the item weights, ordered such that i > j. If such a pair doesn’t exist, return an empty array.
+
+Analyze the time and space complexities of your solution.
+
+Example:
+
+input:  arr = [4, 6, 10, 15, 16],  lim = 21
+*/
+
+function getIndicesOfItemWeights(arr, limit) {
+  let memo = {}
+  
+  for (let i=0; i<arr.length; i++){
+    let complement = limit - arr[i]
+    if (memo[complement] === undefined){
+      memo[arr[i]] = i
+    } else {
+      return [i, memo[complement]]
+    }
+  }
+  
+  return [];
+}
