9fc031d3373af1a138022ff483d1ba4fc722f3e9
Finley Matthews <79953082+finleymatthews96@users.noreply.github.com>
2021-09-03 17:54:28 -0400

    Update BestTimetoBuyandSellStockIII(Array).js

--- BestTimetoBuyandSellStockIII(Array).js
+++ BestTimetoBuyandSellStockIII(Array).js
@@ -60,3 +60,29 @@ function buyAndSellStock(prices, left, right){
 
   return greatest;
 }
+
+// O(n) T | O(1) S bi-directional programming, using an array for best profit from the right, and best from the left
+var maxProfit = function(prices){
+  if (prices.length === 0) return 0;
+  let leftMin = prices[0];
+  let rightMax = prices[prices.length - 1]
+  
+  const leftProfits = Array(prices.length).fill(0);
+  const rightProfits = Array(prices.length + 1).fill(0);
+  
+  for (let i=1; i<prices.length; i++){
+    leftProfits[i] = Math.max(leftProfits[i-1], prices[i] - leftMin);
+    leftMin = Math.min(leftMin, prices[i]);
+    
+    const r = prices.length - 1 - i;
+    rightProfits[r] = Math.max(rightProfits[r+1], rightMax - prices[r])
+    rightMax = Math.max(rightMax, prices[r]);
+  }
+  
+  let maxProfit = 0;
+  for (let i=0; i<prices.length; i++){
+    maxProfit = Math.max(maxProfit, leftProfits[i] + rightProfits[i+1])
+  }
+  
+  return maxProfit;
+}
