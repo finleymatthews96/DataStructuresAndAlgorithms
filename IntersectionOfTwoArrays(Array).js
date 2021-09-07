85558ccfa5f28f1934c431013ddb20c363eccb43
Finley Matthews <79953082+finleymatthews96@users.noreply.github.com>
2021-09-07 14:33:09 -0400

    Update IntersectionOfTwoArrays(Array).js

--- IntersectionOfTwoArrays(Array).js
+++ IntersectionOfTwoArrays(Array).js
@@ -42,3 +42,32 @@ var buildMemo = function(arr){
   
   return memo;
 }
+
+// O(n1 log n1 + n2 log n2) T | O(1) S
+var intersection = function(nums1, nums2) {
+  nums1.sort((a, b) => a - b)
+  nums2.sort((a, b) => a - b)
+  
+  let result = [];
+  
+  let left = 0;
+  let right = 0;
+  
+  while (left < nums1.length && right < nums2.length){
+    while (nums1[left] === nums1[left-1]) left++;
+    while (nums2[right] === nums2[right-1]) right++;
+    if (left >= nums1.length || right >= nums2.length) break
+    
+    if (nums1[left] === nums2[right]){
+      result.push(nums1[left]);
+      left++;
+      right++
+    } else if (nums1[left] > nums2[right]){
+      right++
+    } else {
+      left++
+    }
+  }
+  
+  return result;
+};
