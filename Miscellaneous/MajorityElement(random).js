8806f0f6b48ae3a86f2c3fb666e2adefbb41a312
Finley Matthews <finleymatthews96@gmail.com>
2021-09-22 22:51:59 -0400

    fixed files

--- Miscellaneous/MajorityElement(random).js
+++ Miscellaneous/MajorityElement(random).js
@@ -5,7 +5,7 @@ Given an array nums of size n, return the majority element.
 
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
- 
+
 
 Example 1:
 
@@ -18,38 +18,38 @@ Output: 2
 */
 
 // // O(n) T || O(n) S with memoization
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
   let memo = {};
-  
-  for (let i=0; i<nums.length; i++){
-    if (memo[nums[i]]){
-      memo[nums[i]] += 1
+
+  for (let i = 0; i < nums.length; i++) {
+    if (memo[nums[i]]) {
+      memo[nums[i]] += 1;
     } else {
-      memo[nums[i]] = 1
+      memo[nums[i]] = 1;
     }
     if (memo[nums[i]] > nums.length / 2) return nums[i];
   }
 };
 
 // // O(nlogn) T || O(1) S with sorting
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
   nums.sort((a, b) => a - b);
-  return nums[Math.floor(nums.length / 2)]
+  return nums[Math.floor(nums.length / 2)];
 };
 
 // O(n) T || O(1) S with Boyer-Moore Voting Algorithm - THIS GOES CRAZY
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
   let count = 0;
   let candidate = 0;
-  
-  for (let i=0; i<nums.length; i++){
-    if (count == 0){
+
+  for (let i = 0; i < nums.length; i++) {
+    if (count == 0) {
       candidate = nums[i];
     }
-    count += (nums[i] == candidate) ? 1 : -1
+    count += nums[i] == candidate ? 1 : -1;
   }
-  
-  return candidate;
-}
 
-// CAN ALSO DO DIVIDE AND CONQUER
+  return candidate;
+};
+
+// can also do divide and conquer
