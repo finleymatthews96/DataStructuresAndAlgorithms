55f6d25cb6e029a46a4be0be59e054209bfb8d25
Finley Matthews <79953082+finleymatthews96@users.noreply.github.com>
2021-09-28 12:51:03 -0400

    Update MaximumProfitInJobScheduling(DynProg).js

--- Algorithms/Dynamic Programming/MaximumProfitInJobScheduling(DynProg).js
+++ Algorithms/Dynamic Programming/MaximumProfitInJobScheduling(DynProg).js
@@ -58,3 +58,34 @@ var pickOrDont = function(arr, memo, i){
   memo[i] = Math.max(dont, pick);
   return memo[i]
 }
+
+// O (n log n) T | O(n) S - bottom up dynamic programming
+var jobScheduling = function(startTime, endTime, profit) {
+  const jobs = [];
+  const dp = Array(startTime.length+1).fill(0);
+    
+  for(let i = 0; i < startTime.length; i++) {
+    jobs[i] = [startTime[i], endTime[i], profit[i]];
+  }
+  jobs.sort((a, b) => a[0] - b[0]);
+    
+  function binarySearch(idx) {
+    let low = idx+1, high = jobs.length;
+        
+    while (low < high) {
+      const mid = Math.floor((low + high)/2);
+            
+      if(jobs[mid][0] >= jobs[idx][1]) high = mid;
+      else low = mid + 1;
+    }
+    
+    return low;
+  }
+    
+  for (let i = jobs.length-1; i >= 0; i--) {
+    const next = binarySearch(i)
+    dp[i] = Math.max(dp[i+1],  jobs[i][2] + dp[next])
+  }
+    
+  return dp[0];
+};
